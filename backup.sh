2e16fd34fb60bffa49bd767e15de059dd30e10c3
Mario <mario.suaza.0110@eam.edu.co>
2024-11-17 20:53:30 -0500

    Makefile backup2

--- backup.sh
+++ backup.sh
@@ -2,8 +2,8 @@
 # Variables
 TIMESTAMP=$(date +"%F_%T")
 BACKUP_DIR="./MySQL/backups" # Cambia la ruta según tus necesidades
-MYSQL_USER="portafolio-user"
-MYSQL_PASSWORD="CVEfL3^9CjoRJ#db"
+MYSQL_USER="root"
+MYSQL_PASSWORD="Z1n#*zj10KGo55U%"
 MYSQL_DATABASE="portafolio"
 
 # Crear el directorio de backups si no existe
@@ -11,7 +11,7 @@ mkdir -p $BACKUP_DIR
 
 # Ejecutar el respaldo
 sudo docker exec mysql mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > $BACKUP_DIR/db_backup_$TIMESTAMP.sql
-
+sed -i '/-- Host: localhost/d' $BACKUP_DIR/db_backup_$TIMESTAMP.sql
 # Mantener un solo backup (una vez al día)
 if [[ $(date +"%H") == "23" ]]; then
   echo "Ejecutando limpieza de backups antiguos..."
